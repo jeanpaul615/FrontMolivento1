edaa59bc3d39288064ae632e26c97a85f6bfba30
jeanpaul615 <127168141+jeanpaul615@users.noreply.github.com>
2024-11-21 14:28:09 -0500

    Update Api.js

--- src/containers/Api.js
+++ src/containers/Api.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "http://177.253.13.138:8000/"; // URL universal para todas las APIs.
+export const API_BASE_URL = "http://177.253.13.138:8000"; // URL universal para todas las APIs.
